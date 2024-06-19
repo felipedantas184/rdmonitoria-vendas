8a5395a7db1266ab94952a95ce55cadcce9a5092
Felipe Augusto Oliveira Dantas <felipedantas184@gmail.com>
2024-06-19 20:35:07 -0300

    title

--- pages/aulao-de-matematica.tsx
+++ pages/aulao-de-matematica.tsx
@@ -9,13 +9,13 @@ export default function MentoriaPage() {
         <meta name="description" content="Vem aí o Segundo Aulão de Matemática da RD Monitoria! Uma aula aberta pra gente resolver alguns dos modelos de questões que mais aparecem no ENEM e pra dar dicas de como resolver mais rápid as questões!*" />
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         
-        <meta property="og:title" content="Aulão de Matemátoca | RD Monitoria"/>
+        <meta property="og:title" content="Aulão de Matemática | RD Monitoria"/>
         <meta property="og:type" content="school"/>
         <meta property="og:description" content="Vem aí o Segundo Aulão de Matemática da RD Monitoria! Uma aula aberta pra gente resolver alguns dos modelos de questões que mais aparecem no ENEM e pra dar dicas de como resolver mais rápido as questões!"/>
         <meta property="og:image" content="/apple-touch-icon.png"/>
         <meta property="og:site_name" content="RD Monitoria"/>
 
-        <meta property="twitter:title" content="Aulão de Matemátoca | RD Monitoria"/>
+        <meta property="twitter:title" content="Aulão de Matemática | RD Monitoria"/>
         <meta property="twitter:description" content="Vem aí o Segundo Aulão de Matemática da RD Monitoria! Uma aula aberta pra gente resolver alguns dos modelos de questões que mais aparecem no ENEM e pra dar dicas de como resolver mais rápido as questões!"/>
         <meta property="twitter:image" content="/apple-touch-icon.png"/>
       </Head>
